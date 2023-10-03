c33be4393b4236b6a3e77dbfe492aa0aa5ddd263
henriqueolivgp <henrique.o@ipvc.pt>
2023-10-03 19:54:24 +0100

    Some changes

--- cibereports/src/main.jsx
+++ cibereports/src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './client/routes/App.tsx'
+import App from './client/routes/App'
 import './client/theme/index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
