cf2938e65909b960170deb4537ff5b59172f7ddc
henriqueolivgp <henrique.o@ipvc.pt>
2023-10-13 12:38:21 +0100

    changes

--- CiberReports/src/client/screens/Home.tsx
+++ CiberReports/src/client/screens/Home.tsx
@@ -5,7 +5,7 @@ function Home() {
     <>
       <div className="container mx-auto ">
         <div className="content mx-auto">
-
+          <h1>Home Page</h1>
         </div>
       </div>
     </>
--- CiberReports/src/main.tsx
+++ CiberReports/src/main.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './client/routes/App'
+import Home from './client/screens/Home.tsx'
 import './client/theme/index.css'
+import ErrorPage from './client/screens/ErrorPage.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// importar/configurar o react-router-dom
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import App from './client/routes/App.tsx'
+
+// criação da const function
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,	
+		// Pagina de erro
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			}
+		]
+	}
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
