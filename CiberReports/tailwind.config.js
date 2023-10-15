563d9f170592dc2e32dd1e415f60b599d348020d
henriqueolivgp <henrique.o@ipvc.pt>
2023-10-15 15:19:52 +0100

    tailwind and flowbite configuration

--- CiberReports/tailwind.config.js
+++ CiberReports/tailwind.config.js
@@ -1,11 +1,47 @@
+import flowbitePlugin from 'flowbite/plugin';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [    
-    './src/**/*.{js,jsx,ts,tsx}'
-],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {},
-  },
-  plugins: [],
-}
+    // Define os breakpoints personalizados
+    screens: {
+      'xs': '400px',
+      // => @media (min-width: 470px) { ... }
 
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    fontSize: {
+
+    },
+    colors: {
+
+    },
+    fontFamily: {
+
+    },
+    borderRadius: {
+
+    },
+  },
+  plugins: [
+    flowbitePlugin
+  ],
+}
