1eabd22ff97d26f20e469b34621e2c132a6d8bd8
henriqueolivgp <henrique.o@ipvc.pt>
2023-10-27 14:37:35 +0100

    changes

--- CiberReports/src/client/components/Footer.tsx
+++ CiberReports/src/client/components/Footer.tsx
@@ -3,8 +3,8 @@ function Footer() {
 
     return (
       
-<footer className="bg-white dark:bg-gray-900">
-    <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+<footer className= "mx-10">
+    <div className=" w-full p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
               <a href="https://flowbite.com/" className="flex items-center">
