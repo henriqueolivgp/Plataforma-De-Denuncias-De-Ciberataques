5fb85a17a54e7c8c95611c32c4f919694edd41a2
henriqueolivgp <henrique.o@ipvc.pt>
2023-12-05 22:53:17 +0000

    changes in AuthProvider

--- CiberReports/src/Services/Context/AuthProvider.tsx
+++ CiberReports/src/Services/Context/AuthProvider.tsx
@@ -1,42 +1,50 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState, ReactNode } from "react";
 import { supabase } from "../supabase/client";
+import { User } from "@supabase/supabase-js";
 
 const AuthContext = createContext({});
 
-// eslint-disable-next-line react-refresh/only-export-components
+
 export const useAuth = () => useContext(AuthContext);
 
-const login = (email, password) =>
+const login = (email: string, password: string) =>
   supabase.auth.signInWithPassword({ email, password });
 
 const signOut = () => supabase.auth.signOut();
 
-const passwordReset = (email) =>
+type AuthContextProviderProps = {
+  children: ReactNode
+}
+
+const passwordReset = (email: string) =>
   supabase.auth.resetPasswordForEmail(email, {
     redirectTo: "http://localhost:5173/update-password"
   });
 
-
-const AuthProvider = ({ children }) => {
+const AuthProvider = ({ children }: AuthContextProviderProps ) => {
   const [auth, setAuth] = useState(false);
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
     const { data } = supabase.auth.onAuthStateChange(async (event, session) => {
-      if (event == "PASSWORD_RECOVERY") {
+      if (event === "PASSWORD_RECOVERY") {
         setAuth(false);
       } else if (event === "SIGNED_IN") {
-        setUser(session.user);
-        setAuth(true);
+        // Verificar se 'session' e 'session.user' não são nulos antes de acessá-los
+        if (session && session.user) {
+          setUser(session.user);
+          setAuth(true);
+        }
       } else if (event === "SIGNED_OUT") {
         setAuth(false);
         setUser(null);
       }
     });
     return () => {
-      data.subscription.unsubscribe();
+      data?.subscription.unsubscribe();
     };
   }, []);
+  
 
   return (
     <AuthContext.Provider value={{ auth, user, login, signOut, passwordReset }}>
