fe65e441d95559a89f97808ac8f09a659a4bd855
henriqueolivgp <henrique.o@ipvc.pt>
2023-12-12 17:57:04 +0000

    profile a prova de totos

--- CiberReports/src/client/screens/Profile.tsx
+++ CiberReports/src/client/screens/Profile.tsx
@@ -1,6 +1,23 @@
 import { Banner } from "flowbite-react";
+import { useAuth } from "../../hooks/useAuth";
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
 
 function Profile() {
+  const { user, loading } = useAuth();
+  const navigate = useNavigate();
+
+  // Se o usuário não estiver logado, redirecione-o para a página de login
+  useEffect(() => {
+      if (!loading && !user) {
+          navigate('/signin'); // Substitua pela rota da sua página de login
+      }
+  }, [loading, user, navigate]);
+
+  // Renderize o conteúdo da sua página apenas se o usuário estiver logado
+  if (loading || !user) {
+      return <p>Carregando...</p>;
+  }
   
   return (
     <>
