327407f032faabdaab813a03e2b76fe36a04168a
henriqueolivgp <henrique.o@ipvc.pt>
2023-12-19 20:21:03 +0000

    organizacao de codigo do profile

---
+++ CiberReports/src/client/components/ProfileComponents/Sidebar.tsx
@@ -0,0 +1,79 @@
+import { useLocation, Outlet } from "react-router-dom";
+import { ProfileLi } from "./ProfileLi";
+import { useEffect, useRef, useState } from "react";
+import { useAuth } from "../../../hooks/useAuth";
+import { useNavigate } from 'react-router-dom';
+
+function Explore() {
+
+    const location = useLocation();
+    const { signOut } = useAuth();
+    const navigate = useNavigate();
+
+    const [isOpen, setIsOpen] = useState(false);
+    const menuRef = useRef<HTMLDivElement | null>(null);
+
+    const handleOutsideClick = (event: MouseEvent) => {
+        const target = event.target as Node;
+
+        // Verifica se o clique foi fora do menu
+        if (menuRef.current && !menuRef.current.contains(target)) {
+            setIsOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('click', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('click', handleOutsideClick);
+        };
+    }, []);
+
+    const handleLogOut = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        try {
+            signOut();
+            navigate("/");
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div ref={menuRef}>
+            <button data-drawer-target="default-sidebar" onClick={handleClick} aria-expanded={isOpen} data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="sm:block lg:hidden inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                <span className="sr-only">Open sidebar</span>
+                <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+                </svg>
+            </button>
+            <div className="flex flex-1 ">
+                <aside id="default-sidebar" className={` ${isOpen ? " " : "hidden"} xl:block lg:block llg:hidden w-64 h-auto transition-transform sm:-translate-x-0 lg:translate-x-0  `} aria-label="Sidebar">
+                    <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+                        <ul className="space-y-2 font-medium">
+                            <ProfileLi to="/profile" name="Profile" activeTo={location.pathname} activeLocal={location.pathname} svg={'profile'} />
+                            <ProfileLi to="/profile/edit-profile" name="Edit-Profile" activeTo={location.pathname} activeLocal={location.pathname} svg={'editprofile'} />
+                            <ProfileLi to="/profile/reports" name="Reports" activeTo={location.pathname} activeLocal={location.pathname} svg={'reports'} />
+                            <ProfileLi to="/profile/chat" name="Chat" activeTo={location.pathname} activeLocal={location.pathname} svg={'chat'} />
+                            <ProfileLi to="/profile/settings" name="Settings" activeTo={location.pathname} activeLocal={location.pathname} svg={'settings'} />
+                            <ProfileLi to="#" name="logout" activeTo={location.pathname} activeLocal={location.pathname} svg={'logout'} onClick={handleLogOut} />
+                        </ul>
+                    </div>
+                </aside>
+
+                <div className="flex-1">
+                    <div className="content-box p-4 w-auto">
+                        <Outlet />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Explore;
--- CiberReports/src/client/screens/Profile.tsx
+++ CiberReports/src/client/screens/Profile.tsx
@@ -1,50 +1,23 @@
 import { useAuth } from "../../hooks/useAuth";
-import { useNavigate, Outlet, useLocation } from 'react-router-dom';
-import { useEffect, useRef, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from "react";
 import { format } from "date-fns";
 import { SupaBaseClient } from "../../Services/supabase/SupaBaseClient";
 // Imagens
 import { useImgs } from "../../hooks/useImgs";
 // loading
 import { Loading } from "../components/Loading";
-import { ProfileLi } from "../components/ProfileComponents/ProfileLi";
+
 
 function Profile() {
   const URLBanner = "https://tswdlagzqgorbbabshyx.supabase.co/storage/v1/object/public/Banner/";
   const URLAvatar = "https://tswdlagzqgorbbabshyx.supabase.co/storage/v1/object/public/Avatar/";
   const { bannerImage, getBanner, avatarImage, getAvatar } = useImgs();
-  const { user, signOut } = useAuth();
+  const { user } = useAuth();
   const [profiles, setProfiles] = useState<profile[]>([]);
   const [isLoading, setisLoading] = useState(true);
   const navigate = useNavigate();
-  const location = useLocation();
 
-
-
-  const [isOpen, setIsOpen] = useState(false);
-  const menuRef = useRef<HTMLDivElement | null>(null);
-
-  const handleOutsideClick = (event: MouseEvent) => {
-    const target = event.target as Node;
-
-    // Verifica se o clique foi fora do menu
-    if (menuRef.current && !menuRef.current.contains(target)) {
-      setIsOpen(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('click', handleOutsideClick);
-
-    return () => {
-      document.removeEventListener('click', handleOutsideClick);
-    };
-  }, []);
-
-
-  const handleClick = () => {
-    setIsOpen(!isOpen);
-  };
   const GetAllProfile = async () => {
     const { data } = await SupaBaseClient
       .from('profiles')
@@ -78,17 +51,6 @@ function Profile() {
     id: number;
     all_name: string;
   }
-
-  // Funcao responsavel por fazer o logOut
-  const handleLogOut = async (e: { preventDefault: () => void; }) => {
-    e.preventDefault();
-    try {
-      signOut();
-      navigate("/");
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   let DateJoined: string | undefined;
 
@@ -223,37 +185,6 @@ function Profile() {
               </div>
             </div>
           </div>
-
-          <div ref={menuRef}>
-            <button data-drawer-target="default-sidebar" onClick={handleClick} aria-expanded={isOpen} data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="sm:block lg:hidden inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg  hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-              <span className="sr-only">Open sidebar</span>
-              <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-              </svg>
-            </button>
-            <div className="flex flex-1 ">
-              <aside id="default-sidebar" className={` ${isOpen ? " " : "hidden"} xl:block lg:block llg:hidden w-64 h-auto transition-transform sm:-translate-x-0 lg:translate-x-0  `} aria-label="Sidebar">
-                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-                  <ul className="space-y-2 font-medium">
-                    <ProfileLi to="/profile" name="Profile" activeTo={location.pathname} activeLocal={location.pathname} svg={'profile'} />
-                    <ProfileLi to="/profile/edit-profile" name="Edit-Profile" activeTo={location.pathname} activeLocal={location.pathname} svg={'editprofile'} />
-                    <ProfileLi to="/profile/reports" name="Reports" activeTo={location.pathname} activeLocal={location.pathname} svg={'reports'} />
-                    <ProfileLi to="/profile/chat" name="Chat" activeTo={location.pathname} activeLocal={location.pathname} svg={'chat'} />
-                    <ProfileLi to="/profile/settings" name="Settings" activeTo={location.pathname} activeLocal={location.pathname} svg={'settings'} />
-                    <ProfileLi to="#" name="logout" activeTo={location.pathname} activeLocal={location.pathname} svg={'logout'} onClick={handleLogOut} />
-                  </ul>
-                </div>
-              </aside>
-
-              <div className="flex-1">
-                <div className="content-box p-4 w-auto">
-                  <Outlet />
-                </div>
-              </div>
-            </div>
-          </div>
-
-
 
         </div>
       </div>
