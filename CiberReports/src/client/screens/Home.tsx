f4386656a24d50642958d7f062951eb7de9c3e0c
pedrof04 <metanolpedro@gmail.com>
2023-12-21 19:14:15 +0000

    more_homepage_changes

--- CiberReports/src/client/screens/Home.tsx
+++ CiberReports/src/client/screens/Home.tsx
@@ -1,5 +1,6 @@
 import { useAuth } from "../../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
+import home_banner from "../assets/home_banner.png";
 
 function Home() {
   const { signOut, session } = useAuth();
@@ -259,6 +260,50 @@ function Home() {
           </div>
         </div>
       </div>
+      <section className="banner bg-violet-100 h-auto w-full mb-10 mt-10">
+        <div className="Text-Button flex flex-col relative z-10 xl:flex-row">
+          <div className="w-full absolute overflow-hidden h-auto xl:relative flex items-center content-center">
+            <>
+              <img
+                className="w-screen h-full object-cover"
+                src={home_banner}
+                alt="banner"
+              />
+              <div className="banner-content absolute ml-20">
+                <a className="mb-2 text-2xl font-bold tracking-tight text-white dark:text-white text-justify">
+                  Vamos começar!
+                </a>
+                <p className="mb-3 font-normal text-white dark:text-gray-400 text-justify">
+                  Registe-se aqui para iniciar a sua colaboração connosco e
+                  <br />
+                  ajudar mais pessoas através das suas denúncias.
+                </p>
+                <a
+                  href="/signup"
+                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-bluelite bg-white rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Criar conta
+                  <svg
+                    className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </a>
+              </div>
+            </>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
