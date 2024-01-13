6cebcb3136f932311c9f7eaa513495225528f36e
henriqueolivgp <henrique.o@ipvc.pt>
2024-01-13 15:00:05 +0000

    remove buttons in explore page modal view report

--- CiberReports/src/client/components/ExploreCompunents/ExploreSidebar.tsx
+++ CiberReports/src/client/components/ExploreCompunents/ExploreSidebar.tsx
@@ -263,21 +263,6 @@ function ExploreSidebar() {
                                         </div>
                                     )
                                 })}
-
-                            <div className="flex justify-between">
-                                <button
-                                    type="submit"
-                                    className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                                >
-                                    Update
-                                </button>
-                                <button
-                                    type="submit"
-                                    className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                                >
-                                    Delete User
-                                </button>
-                            </div>
                         </form>
                     </div>
                 </div>
